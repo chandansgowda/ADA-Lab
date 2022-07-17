b20c3e1c4b24d928496453ad842d1034b84b29cb
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-07-17 20:52:20 +0530

    initial commit

---
+++ graphs/floydAlgorithm.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int graph[10][10], n;
+
+void createGraph(){
+	printf("Enter no. of vertices: ");
+	scanf("%d",&n);
+	printf("Enter adjacency matrix:\n");
+	for(int i=0;i<n;i++){
+		for(int j=0; j<n;j++)
+			scanf("%d",&graph[i][j]);
+	}
+}
+
+void main() {
+    int mat[10][10];
+    int i,j,k;
+    
+    createGraph();
+    
+    printf("Cloning graph...\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            mat[i][j] = graph[i][j];
+        }
+    }
+    
+    //Floyd-Warshall Algorithm
+    printf("Applying Floys Warshall Algo...\n");
+    for(k=0;k<n;k++){
+        for(i=0;i<n;i++){
+            for(j=0;j<0;j++){
+                if (mat[i][k] + mat[k][j] < mat[i][j])
+                    mat[i][j] = mat[i][k] + mat[k][j];
+            }
+        }
+    }
+    
+    printf("Shortest path matrix\n");
+    for(i=0;i<n;i++){
+		for(j=0; j<n;j++){
+			printf("%d ",mat[i][j]);
+		}
+		printf("\n");	
+	}
+    
+}
