cbfd78bc5cdd98f9d784d885d8b2de2dae8b5b24
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-07-17 20:57:37 +0530

    Update warshallAlgorithm.c

--- graphs/warshallAlgorithm.c
+++ graphs/warshallAlgorithm.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int graph[10][10], visited[10]={0}, indegree[10]={0}, n;
+int graph[10][10], n;
 
 void createGraph(){
 	printf("Enter no. of vertices: ");
@@ -14,18 +14,18 @@ void createGraph(){
 }
 
 
-
 void main(){
 	int i,j,k, P[10][10];
 	createGraph();
 	
+	printf("Cloning graph...\n");
 	for(i=0;i<n;i++){
 		for(j=0;j<n;j++){
 			P[i][j] = graph[i][j];
 		}
 	}
 	
-	
+	printf("Applying Warshall's Algorithm\n");
 	for(i=0;i<n;i++){
 		for(j=0;j<n;j++){
 			for(k=0;k<n;k++){
