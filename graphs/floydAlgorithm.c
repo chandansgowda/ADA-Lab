c79d9dc831f892a160b419e94beb1bad4de34903
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-07-19 20:28:49 +0530

    Update floydAlgorithm.c

--- graphs/floydAlgorithm.c
+++ graphs/floydAlgorithm.c
@@ -16,12 +16,12 @@ void main() {
     int mat[10][10];
     int i,j,k;
     
-    createGraph();
+    createGraph();  //get input to form matrix for the graph
     
     printf("Cloning graph...\n");
     for(i=0;i<n;i++){
         for(j=0;j<n;j++){
-            mat[i][j] = graph[i][j];
+            mat[i][j] = graph[i][j];  //copying input matrix to temporary matrix mat
         }
     }
     
@@ -30,8 +30,8 @@ void main() {
     for(k=0;k<n;k++){
         for(i=0;i<n;i++){
             for(j=0;j<0;j++){
-                if (mat[i][k] + mat[k][j] < mat[i][j])
-                    mat[i][j] = mat[i][k] + mat[k][j];
+                if (mat[i][k] + mat[k][j] < mat[i][j])  //checking if distance from i->j is lesser via k
+                    mat[i][j] = mat[i][k] + mat[k][j];  //updating the value
             }
         }
     }
@@ -39,7 +39,7 @@ void main() {
     printf("Shortest path matrix\n");
     for(i=0;i<n;i++){
 		for(j=0; j<n;j++){
-			printf("%d ",mat[i][j]);
+			printf("%d ",mat[i][j]); //printing the matrix mat after updating all elements with correcponding sortest distances
 		}
 		printf("\n");	
 	}
